7ececff67dbb9f9b57c8bc87c3a7eb6942240616
Haomin Shi <haominshi@dhcp61.merunorth105.iit.edu>
2018-02-12 14:40:07 -0600

    update

---
+++ multinote/app/src/main/java/com/haomins/www/multinote/Note.java
@@ -0,0 +1,21 @@
+package com.haomins.www.multinote;
+
+/**
+ * Created by haominshi on 2/12/18.
+ */
+
+public class Note {
+
+	private String title;
+	private String content;
+
+	public Note(){
+		//need construct
+		//utlize JSON, create title - content for the note object
+	}
+
+
+
+
+
+}
---
+++ multinote/app/src/main/java/com/haomins/www/multinote/NoteAdapter.java
@@ -0,0 +1,8 @@
+package com.haomins.www.multinote;
+
+/**
+ * Created by haominshi on 2/12/18.
+ */
+
+public class NoteAdapter {
+}
